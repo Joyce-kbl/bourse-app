5d0e96d8367704c388ae269d7ecbad8a66f349a1
mr-leo44 <lionnelkaniki44@gmail.com>
2024-12-16 00:52:26 +0100

    add enum on application model

--- app/Models/Application.php
+++ app/Models/Application.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Enums\Status;
 use App\Models\User;
 use App\Models\Scholarship;
 use Illuminate\Database\Eloquent\Model;
@@ -21,4 +22,7 @@ class Application extends Model
         return $this->belongsTo(Scholarship::class);
     }
 
+    protected $casts = [
+        'status' => Status::class,
+    ];
 }
