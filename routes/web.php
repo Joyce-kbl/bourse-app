77a250792f7fee355e59223d4f8083e7fa95b237
mr-leo44 <lionnelkaniki44@gmail.com>
2024-12-17 02:32:48 +0100

    update home route

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,42 @@
 <?php
 
+use App\Enums\Role;
 use App\Http\Controllers\ProfileController;
+use App\Models\Account;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+  if (Auth::user()) {
+    return redirect()->to('/dashboard');
+  } elseif (User::count() === 0 || Account::where('type_user', Role::ADMIN->value)->count() === 0) {
+    $user = User::create([
+      'name' => 'Admin',
+      'email' => 'admin@ucc.ac.cd',
+      'password' => Hash::make('Admin123@'),
+    ]);
+
+    if ($user) {
+      Account::create([
+        'user_id' => $user->id,
+        'type_user' => Role::ADMIN,
+      ]);
+    }
+    return redirect('login');
+  }
+  return view('welcome');
+})->name('welcome');
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+  return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+  Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+  Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+  Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
